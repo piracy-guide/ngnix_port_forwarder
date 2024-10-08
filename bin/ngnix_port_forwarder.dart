ea528c345a3a0b32d274981fe900c5160f7a9543
xM1haix <kilommm4@gmail.com>
2024-10-08 12:54:08 +0300

    Final version

--- bin/ngnix_port_forwarder.dart
+++ bin/ngnix_port_forwarder.dart
@@ -12,8 +12,23 @@ void main(List<String> args) async {
   final path = "/etc/nginx/sites-available/$subdomain.$domain";
   final file = File(path);
   final exits = await file.exists();
-  if (exits && !confirm) throw "File already exist but it's not forced";
-  await file.writeAsString("""
+  List<String> cmds;
+  if (exits) {
+    if (!confirm) throw "File already exist but it's not forced";
+    print("Updating");
+    final old = await file.readAsLines();
+    for (var i = 0; i < old.length; i++) {
+      if (old[i].contains("proxy_pass http://localhost:")) {
+        old[i] = "proxy_pass http://localhost:$port";
+      }
+    }
+    cmds = [
+      "nginx -t",
+      "systemctl reload nginx",
+    ];
+  } else {
+    print("Creating");
+    await file.writeAsString("""
 server {
     server_name $subdomain.$domain;
     location / {
@@ -25,13 +40,14 @@ server {
     }
 }
 """);
-  final cmds = [
-    "ln -s $path /etc/nginx/sites-enabled/",
-    "nginx -t",
-    "systemctl reload nginx",
-    "certbot --nginx -d $subdomain.$domain",
-    "certbot renew --dry-run",
-  ];
+    cmds = [
+      "ln -s $path /etc/nginx/sites-enabled/",
+      "nginx -t",
+      "systemctl reload nginx",
+      "certbot --nginx -d $subdomain.$domain",
+      "certbot renew --dry-run",
+    ];
+  }
   for (var e in cmds) {
     await CMD(e).run();
   }
